e3054d824390c69da54e0e4588be05b775247815
arlindovs <arlindovicente@gmail.com>
2021-07-19 22:35:25 -0300

    202107192235

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+    <p>Meu primeiro projetinho muito legalzinho!</p>
+    `
+})
+export class MeuPrimeiroComponet {}
