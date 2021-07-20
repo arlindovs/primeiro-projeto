b87ad78a0a2446788d2daa6d952428cf6106627d
arlindovs <arlindovicente@gmail.com>
2021-07-20 19:19:00 -0300

    202107201918

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponet } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuPrimeiroComponet
+    MeuPrimeiroComponet,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
